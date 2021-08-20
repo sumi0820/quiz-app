561f8e54fff57018ca76287de3168afadd082cc5
sumiya_ushiro <u.sumi0820@gmail.com>
2021-08-20 09:29:46 +0900

    Added counting func

--- quiz_app/ViewController.swift
+++ quiz_app/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quizes = [
+        ["One + Two is equal to Three", "True"],
+        ["Three + Four is equal to Six", "False"],
+        ["Five + Six is equal to Eleven", "True"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle ?? "Error"
+        let actualAnswer = quizes[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Correct!")
+        } else {
+            print("Wrong...")
+        }
+        
+        if questionNumber + 1 < quizes.count{
+            questionNumber += 1
+        } else {
+            questionNumber = 0
 
-
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quizes[questionNumber][0]
+        
+    }
 }
 
